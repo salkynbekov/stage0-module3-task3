73f614e5b8b31b1fe5a1229f89a4243b582e3e5b
Damir <salkynbekov.damir@gmail.com>
2023-10-05 21:46:56 +0600

    done2

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -14,11 +14,12 @@ public class ReassigningValues {
         first = 15;
         second = 6;
         third = 4;
-        System.out.println(linkToFirst);
-        System.out.println(linkToSecond);
-        System.out.println(linkToThird);
+
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
